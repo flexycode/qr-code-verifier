b5ad289206f0d15432a01f769919bec389c15e70
0CottonBuds <emiljohnllanes772@gmail.com>
2024-01-21 16:25:50 +0800

    add verifiedPage.js

---
+++ frontend/app/verifiedPage.js
@@ -0,0 +1,9 @@
+import { SafeAreaView, View, Text, Image, Button } from "react-native";
+import {Stack} from "expo-router"
+
+export default function verifiedPage(){
+    <SafeAreaView>
+        
+
+    </SafeAreaView>
+}
